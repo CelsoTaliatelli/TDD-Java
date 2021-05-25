e97eec519b8540a739d7c02f0893e59d0ba9963a
CelsoTaliatelli <celsotaliatelli@hotmail.com>
2021-05-25 12:34:20 -0300

    Refatorando com o Padrão Strategy

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,29 +8,8 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if(desempenho == Desempenho.A_DESEJAR){
-           this.desempenhoADesejar(funcionario);
-        }
-        if(desempenho == Desempenho.BOM){
-            this.desempenhoBom(funcionario);
-        }
-        if(desempenho == Desempenho.OTIMO){
-            this.desempenhoOtimo(funcionario);
-        }
-    }
-
-    public void desempenhoADesejar(Funcionario funcionario){
-        BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-        funcionario.reajustarSalario(reajuste);
-    }
-
-    public void desempenhoBom(Funcionario funcionario){
-        BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-        funcionario.reajustarSalario(reajuste);
-    }
-
-    public void desempenhoOtimo(Funcionario funcionario){
-        BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-        funcionario.reajustarSalario(reajuste);
+       BigDecimal percentual = desempenho.percentualReajuste();
+       BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
+       funcionario.reajustarSalario(reajuste);
     }
 }
